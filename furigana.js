74a57d3f263c84822bd8163755dec16e608243ab
藤野和哉 <fujinokazuya@fujinokazuyanoMacBook-Pro.local>
2018-12-22 04:03:35 +0900

    first commit

---
+++ furigana.js
@@ -0,0 +1,29 @@
+// let hoge = prompt('カラオケのベストスコアは？');
+// while (isNaN(hoge)) {
+//   hoge = prompt('半角数字で入力してください')
+// }
+// if(isNaN(hoge)){
+//   console.log('数値で入力してください');
+// } else {
+//   let huga = prompt('その点数をとった機種は？');
+//   if(huga == 'DAM'){
+//     console.log('気が合いますねぇ');
+//   }else if(huga=='JOY'){
+//     console.log('お前なんて大ッ嫌いだァ！！');
+//   }else {
+//     console.log('私の知らない機種ですねぇ');
+//   }
+// }
+
+// let wday = ['Mon','Tus','Wen','木','金'];
+// for (let cnt of wday){
+//   console.log(wday + '曜日');
+// }
+
+// let createScore = (name) => {
+//   console.log(name+ 'のカラオケ採点を始めます');
+//   base_score = prompt('音程の正確度は何％？');
+//   plus_score = prompt('ロングローンとビブラートの点数は？');
+//   console.log(name+'の結果は'+ (parseInt(base_score)+parseInt(plus_score)/1.4));
+// }
+// createScore('藤野');
